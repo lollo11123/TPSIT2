b74565504378b46a010ccc43af46d6293762affe
lollo11123 <161380557+lollo11123@users.noreply.github.com>
2025-01-10 08:19:48 +0000

    esercizio rettangolo

---
+++ ES07 /RettangoloMain.java
@@ -0,0 +1,25 @@
+class Rettangolo {
+    private  int base; // Attributo immutabile
+    private  int altezza; // Attributo immutabile
+
+    // Costruttore
+    public int Area() {
+        return base * altezza;
+    }
+public void setBase(int base) {
+    this.base = base;
+    }
+public void setAltezza(int altezza) {
+    this.altezza = altezza;
+        }
+    }
+
+   public class RettangoloMain {
+    public static void main(String[] args) {
+        Rettangolo r = new Rettangolo();
+        r.setBase (10);
+        r.setAltezza (20);
+        }
+    }
+
+    
