f1502d75eecc590189e3f89c48956e23b99b9d1f
lollo11123 <161380557+lollo11123@users.noreply.github.com>
2025-05-06 06:44:09 +0000

    confirm

---
+++ Thread/ContatoreMain.java
@@ -0,0 +1,50 @@
+public class ContatoreMain {
+        static class Contatore {
+                private int valore = 0;
+
+                        public synchronized void incrementa() {
+                                    valore++;
+                                            }
+
+                                                    public int getValore() {
+                                                                return valore;
+                                                                        }
+                                                                            }
+
+                                                                                static class ThreadContatore implements Runnable {
+                                                                                        private final Contatore contatore;
+
+                                                                                                public ThreadContatore(Contatore contatore) {
+                                                                                                            this.contatore = contatore;
+                                                                                                                    }
+
+                                                                                                                            @Override
+                                                                                                                                    public void run() {
+                                                                                                                                                for (int i = 0; i < 1000; i++) {
+                                                                                                                                                                contatore.incrementa();
+                                                                                                                                                                            }
+                                                                                                                                                                                    }
+                                                                                                                                                                                        }
+
+                                                                                                                                                                                            public static void main(String[] args) {
+                                                                                                                                                                                                    Contatore contatore = new Contatore();
+                                                                                                                                                                                                            Thread[] threads = new Thread[5];
+
+                                                                                                                                                                                                                    for (int i = 0; i < threads.length; i++) {
+                                                                                                                                                                                                                                threads[i] = new Thread(new ThreadContatore(contatore));
+                                                                                                                                                                                                                                            threads[i].start();
+                                                                                                                                                                                                                                                    }
+
+                                                                                                                                                                                                                                                            for (Thread thread : threads) {
+                                                                                                                                                                                                                                                                        try {
+                                                                                                                                                                                                                                                                                        thread.join();
+                                                                                                                                                                                                                                                                                                    } catch (InterruptedException e) {
+                                                                                                                                                                                                                                                                                                                    e.printStackTrace();
+                                                                                                                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                                                                                                                        }
+
+                                                                                                                                                                                                                                                                                                                                                System.out.println("Valore finale: " + contatore.getValore());
+                                                                                                                                                                                                                                                                                                                                                    }
+                                                                                                                                                                                                                                                                                                                                                    }
+                                                                                                                                                                                                                                                                                                                                                    
+}
---
+++ Thread/EsecuzioneThread.java
@@ -0,0 +1,38 @@
+public class EsecuzioneThread {
+        public static void main(String[] args) {
+                Thread t1 = new ThreadExt("thread 1");
+                        t1.start();
+
+                                Runnable r2 = new RunnableImpl("thread 2");
+                                        Thread t2 = new Thread(r2);
+                                                t2.start();
+
+                                                        new Thread(() -> System.out.println("esecuzione tramite lambda")).start();
+                                                            }
+
+                                                                static class ThreadExt extends Thread {
+                                                                        public ThreadExt(String name) {
+                                                                                    super(name);
+                                                                                            }
+
+                                                                                                    @Override
+                                                                                                            public void run() {
+                                                                                                                        System.out.println("esecuzione tramite ereditarietà: " + getName());
+                                                                                                                                }
+                                                                                                                                    }
+
+                                                                                                                                        static class RunnableImpl implements Runnable {
+                                                                                                                                                private final String name;
+
+                                                                                                                                                        public RunnableImpl(String name) {
+                                                                                                                                                                    this.name = name;
+                                                                                                                                                                            }
+
+                                                                                                                                                                                    @Override
+                                                                                                                                                                                            public void run() {
+                                                                                                                                                                                                        System.out.println("esecuzione tramite runnable: " + name);
+                                                                                                                                                                                                                }
+                                                                                                                                                                                                                    }
+                                                                                                                                                                                                                    }
+                                                                                                                                                                                                                    
+}
